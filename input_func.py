ba803085bb5cc1e83959f21fd34cda08be500a39
MohamedMounier <mohamedmounierabbas@gmail.com>
2023-01-23 21:24:01 +0200

    input function and change str to int

---
+++ input_func.py
@@ -0,0 +1,7 @@
+
+x="400"
+print(f"1 - check type of variable x  :- {type(x)}")
+print(f"2 - change type of str to int :- {type(int(x))}")
+
+age=int(input("write your age :"))
+print(f"your age after 3 years is {age+int(3)}")
