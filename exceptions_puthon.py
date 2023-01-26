1b36b640e793048148d55b3d214892c03cad682e
MohamedMounier <mohamedmounierabbas@gmail.com>
2023-01-26 03:23:50 +0200

    commented name error in Numb2

--- exceptions_puthon.py
+++ exceptions_puthon.py
@@ -1,7 +1,11 @@
+# python error types link
+# https://nouvil.net/python-error-types/
 try:
     number=int(input("put your lucky number : "))
     print(f"lucky number is {number}")
-    numb2
+    #numb2
+    # numb2 will throw a name error as there is no assigned value
+    # value error is when there is a different type expected
 except ValueError:
     print("invalid value kindly put a number.")
     number=int(input("put your lucky number : "))
