9b399ad1c8aff3fd158da0f6a6bfe0a060b6cbc9
MohamedMounier <mohamedmounierabbas@gmail.com>
2023-02-08 14:30:24 +0200

    functions in python

---
+++ functions.py
@@ -0,0 +1,11 @@
+def hello( name):
+    print(f'Hello {name}')
+answer=input('whats your name?')
+
+hello(answer)
+
+def calculate(num1,num2):
+    return num1+num2
+
+result=calculate(5,10)
+print(f"result is {result}")
